feffc13435dc53d847ffb3d8a55677bb6335676a
m-angel2001 <romerohernandezmiguelangel39@gmail.com>
2025-11-06 15:50:06 -0600

    Creacion del Servicio de productos en Angular

---
+++ inventario-app/src/app/servicios/producto.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../model/producto.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductoServicio {
+  
+  private urlBase = "http://localhost:8080/inventario-app";
+
+  constructor(private clienteHttp: HttpClient){}
+
+  obtenerProductosLista(): Observable<Producto[]>{
+    return this.clienteHttp.get<Producto[]>(this.urlBase);
+  }
+}
