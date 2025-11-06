1e6410cba629c39db91cb05d39d46759d723cc37
m-angel2001 <romerohernandezmiguelangel39@gmail.com>
2025-11-05 20:40:54 -0600

    Se agrego la tabla del listado de productos

---
+++ inventario-app/src/app/producto-lista/producto-lista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-lista',
+  imports: [],
+  templateUrl: './producto-lista.html',
+  
+})
+export class ProductoLista {
+
+}
